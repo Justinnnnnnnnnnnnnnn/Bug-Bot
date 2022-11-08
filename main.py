6bf25abeb6a7bfb4ffe9f36bba4f67bb41dd1996
Kennedy Barber <116937764+KennedyBarber@users.noreply.github.com>
2022-11-08 15:26:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('kjkkjkjk')
+import os
+import discord
+import random
+
+TOKEN = ''
+
+client = discord.Client(intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+client.run(TOKEN)
