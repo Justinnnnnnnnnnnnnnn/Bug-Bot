6476ec4d9679cbb4fa140bbc10c103505957fa4e
Justinnnnnnnnnnnnnnn <116762481+Justinnnnnnnnnnnnnnn@users.noreply.github.com>
2022-11-27 22:42:44 -0700

    Update justin.py

--- justin.py
+++ justin.py
@@ -1,1 +1,22 @@
-print("asdjaiushdiauhsdiuhaisudhiu")
+# Dank memes
+import discord
+import random
+import nextcord
+from nextcord.ext import commands
+import json
+import urllib
+
+
+UserInput = commands.Bot(command_prefix= "/")
+
+async def ready():
+    print("When Bot is ready")
+
+
+@UserInput.command()
+async def generateMeme(value):
+    memeAPI = urllib.request.urlopen("https://meme-api.herokuapp.com/gimme")
+
+    memeData = json.load(memeAPI)
+
+    border = nextcord.Embed
