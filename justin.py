e31414fdc7446329f94aea9b657ef6fee4651ba9
Justinnnnnnnnnnnnnnn <116762481+Justinnnnnnnnnnnnnnn@users.noreply.github.com>
2022-11-28 22:18:39 -0700

    Update justin.py

--- justin.py
+++ justin.py
@@ -1,32 +1,26 @@
 # Dank memes
 import discord
-import random
-import nextcord
-from nextcord.ext import commands
+from discord.ext import commands
+from requests import get
 import json
-import urllib
+
+client = commands.Bot(intents = discord.Intents.all(), command_prefix = '/meme')
 
 
-UserInput = commands.Bot(command_prefix= "/meme")
+@client.command()
+async def test(ctx):
+   await ctx.reply("Testing")
 
-# async def ready():
-#     print("When Bot is ready")
+   
+@client.command()
+async def Memes(ctx):
+    content = get("https://meme-api.herokuapp.com/gimme").text # API
+
+    data = json.loads(content,)
+
+    memeData = discord.Embed(title=f"{data['title']}", Color = discord.Color.random()).set_image(url=f"{data['url']}")
+
+    await ctx.reply(embed=memeData)
 
 
-@UserInput.command()
-async def generateMeme(run):
-    memeAPI = urllib.request.urlopen("https://meme-api.herokuapp.com/gimme")
-
-    memeData = json.load(memeAPI)
-
-    memeURL = memeData["title"]
-
-    memeName = memeData["url"]
-
-
-    border = nextcord.Embed(title = memeName, color = nextcord.colour.gray())
-
-    border.set_image(url = memeURL)
-
-    await run.send(border = border)
-
+client.run('MTAzOTY3MTMyNjc5NDg0NjMwOA.GY8ybo.dSFdoRJelBYB6lAveGTVYTmisFfsuqzDJAwct8') # Needs the token
