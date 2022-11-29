4110e3854c31d421272c14fc56c20e3cc268a6a3
Kennedy Barber <116937764+KennedyBarber@users.noreply.github.com>
2022-11-28 23:35:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,30 +9,30 @@ TOKEN = 'MTAzOTY3MTMyNjc5NDg0NjMwOA.GY8ybo.dSFdoRJelBYB6lAveGTVYTmisFfsuqzDJAwct
 
 client = discord.Client(intents=discord.Intents.all())
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
 
 bot = commands.Bot(intents=discord.Intents.all(), command_prefix = '/')
 
 
-client = commands.Bot(intents = discord.Intents.all(), command_prefix = '/meme')
+client_1 = commands.Bot(intents = discord.Intents.all(), command_prefix = '/meme')
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+# @client_1.command()
+# async def test(ctx):
+#    await ctx.reply("Testing")
 
 
-@client.command()
-async def test(ctx):
-   await ctx.reply("Testing")
+# @client_1.command()
+# async def Memes(ctx):
+#     content = get("https://meme-api.herokuapp.com/gimme%22").text 
 
+#     data = json.loads(content,)
 
-@client.command()
-async def Memes(ctx):
-    content = get("https://meme-api.herokuapp.com/gimme%22").text 
+#     memeData = discord.Embed(title=f"{data['title']}", Color = discord.Color.random()).set_image(url=f"{data['url']}")
 
-    data = json.loads(content,)
-
-    memeData = discord.Embed(title=f"{data['title']}", Color = discord.Color.random()).set_image(url=f"{data['url']}")
-
-    await ctx.reply(embed=memeData)
+#     await ctx.reply(embed=memeData)
 
 
 @client.event
@@ -43,7 +43,17 @@ async def on_message(message):
     channel = str(message.channel.name)
     print(f'{username}: {user_message} ({channel})')
 
-  
+
+    if message.content.startswith('/meme'):
+        content = get("https://meme-api.herokuapp.com/gimme%22").text 
+
+        data = json.loads(content,)
+
+        memeData = discord.Embed(title=f"{data['title']}", Color = discord.Color.random()).set_image(url=f"{data['url']}")
+
+        await message.channel.send(embed=memeData)
+
+
     if message.author == client.user:
         return
     if message.content.startswith('/quiz'):
@@ -161,12 +171,109 @@ async def on_message(message):
         elif count > 15:
             role = 'Dragonfly'
         elif count > 10:
-            role = 'Buttlerfly'
+            role = 'Butterfly'
         elif count <= 10:
             role = 'Ant'
 
         await message.channel.send(f"Congrats!! You've been selected for the {role} role")
         give_role = discord.utils.get(message.guild.roles, name= role)
         await message.author.edit(roles=[give_role])
+
+    if message.content.startswith('/madlib'):
+        channel = message.channel
+        await channel.send('Welcome to mab libs! You will be asked to give a number of nouns, adjectives, verbs, and more. At the end, everything you pick will be put together into a customized story. Press (x) to continue. Enter quit at any time to stop.')
+        storeInp = []
+
+        def check(m):
+            return m.content == 'x'
+
+        m = await client.wait_for('message', check=check)
+        # got user input showing to start.
+        await message.channel.send('Enter an adjective.')
+
+        def check(m):
+            return m.content != 'quit'
+        
+        m = await client.wait_for('message', check=check)
+        if m.content != 'quit':
+            storeInp.append(m.content)
+
+        await message.channel.send('Enter a verb.')
+        m = await client.wait_for('message', check=check)
+        if m.content != 'quit':
+            storeInp.append(m.content)
+            
+        await message.channel.send('Enter a noun.')
+        m = await client.wait_for('message', check=check)
+        if m.content != 'quit':
+            storeInp.append(m.content)
+            
+        await message.channel.send('Enter an adjective.')
+        m = await client.wait_for('message', check=check)
+        if m.content != 'quit':
+            storeInp.append(m.content)
+
+        await message.channel.send('Enter a noun.')
+        m = await client.wait_for('message', check=check)
+        if m.content != 'quit':
+            storeInp.append(m.content)
+            
+        await message.channel.send('Enter a verb.')
+        m = await client.wait_for('message', check=check)
+        if m.content != 'quit':
+            storeInp.append(m.content)
+            
+        await message.channel.send('Enter a number.')
+        m = await client.wait_for('message', check=check)
+        if m.content != 'quit':
+            storeInp.append(m.content)
+            
+        await message.channel.send('Enter a noun.')
+        m = await client.wait_for('message', check=check)
+        if m.content != 'quit':
+            storeInp.append(m.content)
+            
+
+        await message.channel.send('Enter a honorific.')  # ???
+        m = await client.wait_for('message', check=check)
+        if m.content != 'quit':
+            storeInp.append(m.content)
+
+        await message.channel.send('Enter a name.')
+        m = await client.wait_for('message', check=check)
+        if m.content != 'quit':
+            storeInp.append(m.content)
+
+        await message.channel.send('Enter a verb.')
+        m = await client.wait_for('message', check=check)
+        if m.content != 'quit':
+            storeInp.append(m.content)
+            
+        await message.channel.send('Enter a verb ending in -ing.')
+        m = await client.wait_for('message', check=check)
+        if m.content != 'quit':
+            storeInp.append(m.content)
+
+        await message.channel.send('Enter a verb.')
+        m = await client.wait_for('message', check=check)
+        if m.content != 'quit':
+            storeInp.append(m.content)
+
+        await message.channel.send('Enter a singular noun.')  # or change to body part
+        m = await client.wait_for('message', check=check)
+        if m.content != 'quit':
+            storeInp.append(m.content)
+            
+        await message.channel.send('Enter a verb.')
+        m = await client.wait_for('message', check=check)
+        if m.content != 'quit':
+            storeInp.append(m.content)
+            
+        await message.channel.send('Enter a number.')
+        m = await client.wait_for('message', check=check)
+        if m.content != 'quit':
+            storeInp.append(m.content)
+            
+        await message.channel.send(f'Just another {storeInp[0]} day as i {storeInp[1]} at my computer. Yet again my {storeInp[2]} refused to work. It should be {storeInp[3]}, but no, my {storeInp[4]} never wants to {storeInp[5]}. Even after {storeInp[6]} hours, I still keep getting {storeInp[7]} errors. My teacher, {storeInp[8]} {storeInp[9]} said I just need to {storeInp[10]} but my code still keeps {storeInp[11]}. I am about to {storeInp[12]} my {storeInp[13]} {storeInp[14]} if it doesn\'t work after {storeInp[15]} more tries.')
 
 client.run(TOKEN)
