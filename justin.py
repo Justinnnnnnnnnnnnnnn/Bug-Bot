3e8f31545baffbc4fbfea1ff3b198d501dcf4159
Justinnnnnnnnnnnnnnn <116762481+Justinnnnnnnnnnnnnnn@users.noreply.github.com>
2022-11-29 17:55:17 -0700

    Update justin.py

---
+++ justin.py
@@ -0,0 +1,66 @@
+elif message.content.startswith('/sus'):
+        channel = message.channel
+        await channel.send('_ ඞ')
+
+elif message.content.startswith('/lenny'):
+        channel = message.channel
+        await channel.send('( ͡• ͜ʖ ͡•)')
+
+
+elif message.content.startswith('/happy'):
+        channel = message.channel
+        await channel.send('(´･ᴗ･`)')
+
+
+elif message.content.startswith('/hap'):
+        channel = message.channel
+        await channel.send('(ʘ‿ʘ)')
+
+
+
+elif message.content.startswith('/shrug'):
+        channel = message.channel
+        await channel.send('¯\_(⊙_ʖ⊙)_/¯')
+
+
+
+elif message.content.startswith('/watching'):
+        channel = message.channel
+        await channel.send('(ʘ ل͟├┬┴┬┴')
+
+elif message.content.startswith('/look'):
+        channel = message.channel
+        await channel.send(':eye::lips::eye:')
+
+
+elif message.content.startswith('/studying'):
+        channel = message.channel
+        await channel.send('https://tenor.com/view/studio-ghibli-the-wind-rises-windy-flying-paper-writing-gif-17091335')
+
+
+
+elif message.content.startswith('/cs'):
+        channel = message.channel
+        await channel.send('https://tenor.com/view/computer-science-scravers-gif-24687936')
+
+
+elif message.content.startswith('/cope'):
+        channel = message.channel
+        await channel.send('https://tenor.com/view/copium-meme-pepe-frog-gif-26117735')
+
+
+elif message.content.startswith('/over'):
+        channel = message.channel
+        await channel.send('https://tenor.com/view/kermit-falling-building-fall-woodz-gif-24715383')
+
+
+elif message.content.startswith('/ok'):
+        channel = message.channel
+        await channel.send('https://tenor.com/view/crycat-crying-cat-crying-cat-thumbs-up-thumbs-up-ok-gif-22851318')
+
+
+elif message.content.startswith('/bonk'):
+        channel = message.channel
+        await channel.send('https://tenor.com/view/bonk-doge-gif-24837098')
+
+
