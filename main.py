77db8d3b0348de39fd84b61a8757721e8b7d9bf5
Justinnnnnnnnnnnnnnn <116762481+Justinnnnnnnnnnnnnnn@users.noreply.github.com>
2022-11-29 20:40:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import os
 import discord
 import random
 from discord.ext import commands
-from requests import get
-import json
 import pandas as pd
 from datetime import datetime
 from words import *
