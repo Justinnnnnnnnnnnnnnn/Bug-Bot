f4c6f665f87fd98e46fb59bfa533884e4f417e6f
Justinnnnnnnnnnnnnnn <116762481+Justinnnnnnnnnnnnnnn@users.noreply.github.com>
2022-11-29 17:56:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -278,4 +278,70 @@ async def on_message(message):
             
         await message.channel.send(f'Just another {storeInp[0]} day as i {storeInp[1]} at my computer. Yet again my {storeInp[2]} refused to work. It should be {storeInp[3]}, but no, my {storeInp[4]} never wants to {storeInp[5]}. Even after {storeInp[6]} hours, I still keep getting {storeInp[7]} errors. My teacher, {storeInp[8]} {storeInp[9]} said I just need to {storeInp[10]} but my code still keeps {storeInp[11]}. I am about to {storeInp[12]} my {storeInp[13]} {storeInp[14]} if it doesn\'t work after {storeInp[15]} more tries.')
 
+    elif message.content.startswith('/sus'):
+        channel = message.channel
+        await channel.send('_ ඞ')
+
+    elif message.content.startswith('/lenny'):
+            channel = message.channel
+            await channel.send('( ͡• ͜ʖ ͡•)')
+
+
+    elif message.content.startswith('/happy'):
+            channel = message.channel
+            await channel.send('(´･ᴗ･`)')
+
+
+    elif message.content.startswith('/hap'):
+            channel = message.channel
+            await channel.send('(ʘ‿ʘ)')
+
+
+    elif message.content.startswith('/shrug'):
+            channel = message.channel
+            await channel.send('¯\_(⊙_ʖ⊙)_/¯')
+
+
+
+    elif message.content.startswith('/watching'):
+            channel = message.channel
+            await channel.send('(ʘ ل͟├┬┴┬┴')
+
+    elif message.content.startswith('/look'):
+            channel = message.channel
+            await channel.send(':eye::lips::eye:')
+
+
+    elif message.content.startswith('/studying'):
+            channel = message.channel
+            await channel.send('https://tenor.com/view/studio-ghibli-the-wind-rises-windy-flying-paper-writing-gif-17091335')
+
+
+
+    elif message.content.startswith('/cs'):
+            channel = message.channel
+            await channel.send('https://tenor.com/view/computer-science-scravers-gif-24687936')
+
+
+    elif message.content.startswith('/cope'):
+            channel = message.channel
+            await channel.send('https://tenor.com/view/copium-meme-pepe-frog-gif-26117735')
+
+
+    elif message.content.startswith('/over'):
+            channel = message.channel
+            await channel.send('https://tenor.com/view/kermit-falling-building-fall-woodz-gif-24715383')
+
+
+    elif message.content.startswith('/ok'):
+            channel = message.channel
+            await channel.send('https://tenor.com/view/crycat-crying-cat-crying-cat-thumbs-up-thumbs-up-ok-gif-22851318')
+
+
+    elif message.content.startswith('/bonk'):
+            channel = message.channel
+            await channel.send('https://tenor.com/view/bonk-doge-gif-24837098')
+
+
+
 client.run(TOKEN)
